b303f8aae9fd5b5aa0320b96418e7ce85013621a
Michael Fürst <penguinmenac3@gmail.com>
2014-06-20 16:36:03 +0200

    Added a loop to draw stuff and update stuff

---
+++ src/actors/actor.rb
@@ -0,0 +1,25 @@
+class Actor
+  attr_reader :type
+  attr_reader :x
+  attr_reader :y
+   
+  def initialize window, img, sound, type
+    @image = Gosu::Image.new(window, img, false)
+    @beep = Gosu::Sample.new(window, sound)
+    @x = @y = @vel_x = @vel_y = @angle = 0.0
+    @score = 0
+    @type = type
+    window.addActor self
+  end
+
+  def warp(x, y)
+    @x, @y = x, y
+  end
+
+  def update elapsed_time = 0.16
+  end
+
+  def draw
+    @image.draw_rot(@x, window.height / 2 - @y, ZOrder::Player, @angle)
+  end
+end
---
+++ src/actors/cat.rb
@@ -0,0 +1,55 @@
+class Cat < Actor
+
+  attr_reader :speed
+
+  def initialize window, img, sound, terrain
+    super window, img, sound, "cat"
+
+    @gravity = -9.81 # trololo
+  	@accelerate = 0
+  	self.warp 20, 0
+  	@speed = 1.0
+  	@terrain = terrain
+
+    # cat is ready
+    puts "miau"
+  end
+
+  def update elapsed_time = 0.16
+  	prev_y = self.y
+  	self.vel_y = self.vel_y + @gravity * elapsed_time
+  	self.vel_y = self.vel_y - @accelerate
+  	self.y = self.vel_y * elapsed_time + self.y
+
+    # collide with terrain
+  	if self.y < @terrain.terrainBuffer[self.x]
+      self.y = @terrain.terrainBuffer[self.x]
+  	end
+
+  	# update velocity to actual velocity.
+  	self.vel_y = prev_y - self.y
+  end
+
+  # modify the speed required for effects
+  def modify_speed modifier
+  	@speed = @speed * modifier
+  end
+
+  # flip a cat (required for the flip paradoxon)
+  def flip
+    self.angle =  self.angle + 180
+  end
+
+  def accelerate_down active = true
+  	if active
+      @accelerate = 1
+    else
+	  @accelerate = 0
+    end
+  end
+
+  def eaten_by_alf
+  	puts "*fauch*"
+  	puts "iiiek"
+  end
+end
