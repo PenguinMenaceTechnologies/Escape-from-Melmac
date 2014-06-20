05f51527d2143f7b7d87de391a76e6b2523b2a36
joschuck <jojoschuck@googlemail.com>
2014-06-20 17:04:40 +0200

    optimized waves

--- src/map/terrain.rb
+++ src/map/terrain.rb
@@ -1,8 +1,8 @@
 
 module MagicNumbers
 	BUFFER_SIZE = 2560
-	MIN_AMP, MAX_AMP = 0.5, 1.5
-	MIN_PHASE, MAX_PHASE = 80, 250
+	MIN_AMP, MAX_AMP = 0.7, 1.3
+	MIN_PHASE, MAX_PHASE = 100, 250
 end
 class Terrain
 	attr_reader :terrainBuffer
@@ -21,9 +21,9 @@ class Terrain
 			currentPhase = rand(MagicNumbers::MIN_PHASE..MagicNumbers::MAX_PHASE)
 			puts currentPhase
 			currentAmplitude = rand(MagicNumbers::MIN_AMP..MagicNumbers::MAX_AMP)
-			i = range
-			while i < range + currentPhase do
-				@terrainBuffer[i] = currentAmplitude * Math::cos((2 * Math::PI / currentPhase) * i)
+			i = 0
+			while i < currentPhase do
+				@terrainBuffer[range + i] = currentAmplitude * Math::sin((2 * Math::PI / currentPhase) * i)
 				i += 1
 			end
 
@@ -35,7 +35,7 @@ class Terrain
 		red = Gosu::Color.argb(0xffff0000)
 		i = 0
 		while i < window.width do
-			window.draw_line(i, window.height, red, i, window.height / 2 - (@terrainBuffer[i] * window.height / 3), red)
+			window.draw_line(i, window.height, red, i, window.height / 1.5 - (@terrainBuffer[i] * window.height / 4), red)
 			i += 1
 		end
 	end
